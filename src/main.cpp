c0a2790e23b7c1ee7676e7f955459cf8741c335a
Alvin Thai <athai005@ucr.edu>
2015-04-14 12:09:46 -0700

    edited parsing algorithm.

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <string>
 #include <boost/tokenizer.hpp>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 using namespace std;
 using namespace boost;
 
@@ -32,9 +34,10 @@ void commandexec(vector<string> x)
 			}
 			else if (pid == 0) //if pid is 0, you are in child process. execvp here.
 			{
-				pos++;
-				execvp("ls", x);
+//				execvp(x.at(pos), x.at(pos+1)); 
 			}
+			else
+				wait(0);
 		}
 	}
 }
@@ -45,20 +48,22 @@ int main (int argc, char **argv)
 
 	//bool value use to determine when to kill rshell loop
 	bool killrshell = false; 
-	
 
 	while (!killrshell)
 	{
 
+		//bool will store syntax error
+		bool synerror = false;
 		//string will store raw user input
 		string rawinput;
-		
+		//vector of strings, will convert to array for execvp
+		vector<string> cmds;
 	
 		//simple command prompt print
 		printf("%s", "$ ");
 
 		//obtain user input commands
-		std::getline(std::cin, rawinput);
+		getline(cin, rawinput);
 
 		//temporary break loop condition: user inputs 'exit' 
 		if (rawinput.substr(0,4) == "exit") 
@@ -69,25 +74,76 @@ int main (int argc, char **argv)
 
 		//parse through raw input and remove comments if entered
 		//removes everything after '#'
-		if (rawinput.find('#') != std::string::npos) rawinput = rawinput.substr(0, rawinput.find('#'));
+		if (rawinput.find('#') != string::npos) rawinput = rawinput.substr(0, rawinput.find('#'));
 	
-		
 		//TODO: Parse raw input string for commands and connectors
-		char_separator<char> delim(" -", "|");
+		char_separator<char> delim(" ", "|");
 		tokenizer< char_separator<char> > mytok(rawinput);
 
 		
-		//tokenizer test: storing tokenized objects in a vector
-		vector<string> parsedinput;
+		//tokenizer test: storing tokenized objects in a string
+		string parsedinput;
 		for(auto it = mytok.begin(); it != mytok.end(); ++it)
 		{
-			parsedinput.push_back(*it);
+			parsedinput += *it;
 		}
+		parsedinput += ";";
 		//token test output loop
-		for (int i=0; i < parsedinput.size(); i++)
-			cout << parsedinput.at(i) << ' ';
+		cout << parsedinput << endl;
 		
-		cout << endl;
+		//initial scan for syntax errors
+		if (!isalpha(parsedinput.at(0))) synerror = true;
+
+		//bool variable to tell when the inner for loop is done
+		bool seploopdone = false;
+
+		//iterate through string and separate into commands and connectors
+		while (synerror != true && seploopdone == false)
+		{
+			string parsecmd;
+			for (int i=0; i<parsedinput.size(); i++)
+			{
+				if (i >= parsedinput.size()) break;
+
+				if ( (parsedinput.at(i) == '&' || parsedinput.at(i) == '|') && parsedinput.at(i+1) == parsedinput.at(i) )
+				{
+					if (parsedinput.at(i+2) == parsedinput.at(i))
+					{
+						synerror =true;
+						break;
+					}
+					else
+					{
+						string con1;
+						con1 += parsedinput.at(i);
+						string con2;
+						con2 += parsedinput.at(i+1);
+						string constring = con1 + con2;
+						cmds.push_back(constring);
+						parsecmd.clear();
+						i+=1;
+
+						cout << "connector added" << endl;
+					}
+				}
+				else 
+				{
+					parsecmd += parsedinput.at(i);
+					if (!isalpha(parsedinput.at(i+1)))
+					{
+						cmds.push_back(parsecmd);
+						parsecmd.clear();
+
+						cout << "command added" << endl;
+					}
+				}
+			}
+			seploopdone = true;
+		}
+
+		//PARSING TEST OUTPUT
+		for (int i=0; i<cmds.size(); i++)
+			cout << cmds.at(i) << endl;
 	
 		//temporary input handling -- if exit was not entered, output the input.
 		printf("\"%s\" was entered.\n", rawinput.c_str());
