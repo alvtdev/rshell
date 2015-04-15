2338798095a70d5a5517029b37d75e0216bfa776
Alvin Thai <athai005@ucr.edu>
2015-04-15 12:40:10 -0700

    debugging. attempting new approach.

--- src/main.cpp
+++ src/main.cpp
@@ -2,46 +2,16 @@
 #include <cstdio>
 #include <cstdlib>
 #include <string>
-#include <boost/tokenizer.hpp>
+#include <vector>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 using namespace std;
-using namespace boost;
-
 
 
 /*TODO: refine the commandexec algorithm before proceeding further.
 This current algorithm was an initial attempt.
 It does not work, and will be replaced. */
-
-
-//function for iterating through parsed string
-void commandexec(vector<string> x)
-{
-	//position of iterator
-	int pos = 0;
-	while (pos < x.size())
-	{
-		//if 'ls' is found
-		if (x.at(pos) == "ls")
-		{
-			int pid = fork();
-			if (pid == -1) //if pid is -s, an error has occurred.
-			{
-				perror("fork");
-				exit(1);
-			}
-			else if (pid == 0) //if pid is 0, you are in child process. execvp here.
-			{
-//				execvp(x.at(pos), x.at(pos+1)); 
-			}
-			else
-				wait(0);
-		}
-	}
-}
-
 
 int main (int argc, char **argv)
 {
@@ -75,92 +45,103 @@ int main (int argc, char **argv)
 		//parse through raw input and remove comments if entered
 		//removes everything after '#'
 		if (rawinput.find('#') != string::npos) rawinput = rawinput.substr(0, rawinput.find('#'));
-
-		//variables to keep track of whitespaces
-		int wsbegin = 0;
-		int wsend = 0;
-		string parsedinput = rawinput;
-
-
-		//first remove leading whitespaces at the very beginning of the string
-		while (parsedinput.at(wsend) == ' ') wsend++;
-		parsedinput = parsedinput.substr(wsend, parsedinput.size()-1);
-		//then reinit wsend for removing inner whitespaces
-		wsend = 0;
-
-		//next remove trailing whitespaces at the end of the string
-		wsbegin = parsedinput.size()-1;
-		if(parsedinput.at(wsbegin) == ' ')
-		{
-			while (parsedinput.at(wsbegin) == ' ')
-				--wsbegin;
 		
-			parsedinput = parsedinput.substr(0, wsbegin+1);
-		}
-		//then reinit wsbegin for removing inner whitespaces 
-		wsbegin = 0;
 
-		//remove extra whitespaces in the middle of the string
-		for (int i=0; i<parsedinput.size(); i++) 
+		//execute the following code ONLY IF STRING IS NOT EMPTY
+		if (!rawinput.empty())
 		{
-		}
-			
+				
+			//variables to keep track of whitespaces
+			int wsbegin = 0;
+			int wsend = 0;
+			string parsedinput = rawinput;
 
 
-		parsedinput += ';';
-		cout << "new input:" << parsedinput << endl;
-		
-		//initial scan for syntax errors
-		if (!isalpha(parsedinput.at(0))) synerror = true;
+			//first remove leading whitespaces at the very beginning of the string
+			while (parsedinput.at(wsend) == ' ') wsend++;
+			parsedinput = parsedinput.substr(wsend, parsedinput.size());
+			//then reinit wsend for removing inner whitespaces
+			wsend = 0;
 
-		//bool variable to tell when the inner for loop is done
-		bool seploopdone = false;
-
-		//iterate through string and separate into commands and connectors
-		while (synerror != true && seploopdone == false)
-		{
-			string parsecmd;
-			for (int i=0; i<parsedinput.size(); i++)
+			//next remove trailing whitespaces at the end of the string
+			parsedinput += ' '; //add space at the end to make removal easier.
+			wsbegin = parsedinput.size()-1;
+			if(parsedinput.at(wsbegin) == ' ')
 			{
-				if (i >= parsedinput.size()-1) break; //accomodates for concatenated ; at the end of the string
-				//checks for & and |
-				if ( (parsedinput.at(i) == '&' || parsedinput.at(i) == '|') && parsedinput.at(i+1) == parsedinput.at(i) )
+				while (parsedinput.at(wsbegin) == ' ')
 				{
-					//if 3 &&& |||, or 1 & | -- syntax error
-					if (parsedinput.at(i+2) == parsedinput.at(i))
-					{
-						synerror =true;
-						break;
-					}
-					else if (parsedinput.at(i+1) != parsedinput.at(i))
-					{
-						synerror=true;
-						break;
-					}
-					else
-					{
-						string con1;
-						con1 += parsedinput.at(i);
-						string con2;
-						con2 += parsedinput.at(i+1);
-						string constring = con1 + con2;
-						cmds.push_back(constring);
-						parsecmd.clear();
-						i++;
-					}
+					--wsbegin;
 				}
-				else 
+			
+				parsedinput = parsedinput.substr(0, wsbegin+1);
+			}
+			//then reinit wsbegin for removing inner whitespaces 
+			wsbegin = 0;
+
+			//remove extra whitespaces in the middle of the string
+			for (int i=0; i<parsedinput.size(); i++) 
+			{
+				if (parsedinput.at(i) == ' ' && wsbegin ==0) wsbegin = i;
+				if (parsedinput.at(i) != ' ' && wsbegin > 0) wsend = i;
+				else if (wsend > wsbegin)
 				{
-					parsecmd += parsedinput.at(i);
-					if (!isalpha(parsedinput.at(i+1)))
-					{
-						cmds.push_back(parsecmd);
-						parsecmd.clear();
-					}
+					parsedinput = parsedinput.substr(0, wsbegin) + parsedinput.substr(wsend, -1);
+					i-=(wsend-wsbegin);
+					wsbegin = 0;
+					wsend = 0;
 				}
 			}
-			if(synerror = true) break;
-			seploopdone = true;
+
+			parsedinput += ';';
+			cout << "new formatted input:" << parsedinput << endl;
+			
+			//initial scan for syntax errors
+			if (!isalpha(parsedinput.at(0))) synerror = true;
+
+			//bool variable to tell when the inner for loop is done
+			bool seploopdone = false;
+
+			//iterate through string and separate into commands and connectors
+			while (synerror != true && seploopdone == false)
+			{
+				string parsecmd;
+				for (int i=0; i<parsedinput.size(); i++)
+				{
+					if (i >= parsedinput.size()-1) break; //accomodates for concatenated ; at the end of the string
+					//checks for & and |
+					if ( (parsedinput.at(i) == '&' || parsedinput.at(i) == '|') && parsedinput.at(i+1) == parsedinput.at(i) )
+					{
+						//if 3 &&& ||| -- syntax error
+						if (parsedinput.at(i+2) == parsedinput.at(i))
+						{
+							synerror =true;
+							break;
+						}
+						else
+						{
+							string con1;
+							con1 += parsedinput.at(i);
+							string con2;
+							con2 += parsedinput.at(i+1);
+							string constring = con1 + con2;
+							cmds.push_back(constring);
+							parsecmd.clear();
+							i++;
+						}
+					}
+					else 
+					{
+						parsecmd += parsedinput.at(i);
+						if (!isalpha(parsedinput.at(i+1)))
+						{
+							cmds.push_back(parsecmd);
+							parsecmd.clear();
+						}
+					}
+				}
+				if(synerror = true) break;
+				seploopdone = true;
+			}
 		}
 
 		//PARSING TEST OUTPUT
